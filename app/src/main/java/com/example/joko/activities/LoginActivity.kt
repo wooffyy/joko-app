943f3f2e3fb941b653d084419cd75fb582a10521
Ray <raymond4114p@gmail.com>
2026-05-19 09:10:34 +0700

    fix : memperbaiki fungsi tombol hide/show passord yang tidak bekerja

--- app/src/main/java/com/example/joko/activities/LoginActivity.kt
+++ app/src/main/java/com/example/joko/activities/LoginActivity.kt
@@ -10,7 +10,7 @@ import com.example.joko.R
 
 class LoginActivity : AppCompatActivity() {
     private var isPasswordVisible = false
-
+//testo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
