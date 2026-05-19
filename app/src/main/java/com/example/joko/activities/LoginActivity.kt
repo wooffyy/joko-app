69177ccc9116d6198f771332a6c824e528458bcb
Ray <raymond4114p@gmail.com>
2026-05-19 08:43:44 +0700

    fix : memperbaiki fungsi tombol hide/show passord yang tidak bekerja

--- app/src/main/java/com/example/joko/activities/LoginActivity.kt
+++ app/src/main/java/com/example/joko/activities/LoginActivity.kt
@@ -1,12 +1,36 @@
 package com.example.joko.activities
 
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.joko.R
 
 class LoginActivity : AppCompatActivity() {
+    private var isPasswordVisible = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val ivShowPassword = findViewById<ImageView>(R.id.ivShowPassword)
+
+        ivShowPassword.setOnClickListener {
+            if (isPasswordVisible) {
+                // Sembunyikan Password
+                etPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                isPasswordVisible = false
+            } else {
+                // Tampilkan Password
+                etPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                isPasswordVisible = true
+            }
+            
+            // Pindahkan kursor ke posisi paling akhir teks
+            etPassword.setSelection(etPassword.text.length)
+        }
     }
 }
