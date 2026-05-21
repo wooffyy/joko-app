a412dbfe15946125d2f0ab64ff70feb10bf48af0
wooffyy <wahfiuddin3173@gmail.com>
2026-05-21 12:14:04 +0700

    fix: minor fix on AuthResponse

--- app/src/main/java/com/example/joko/data/remote/response/AuthResponse.kt
+++ app/src/main/java/com/example/joko/data/remote/response/AuthResponse.kt
@@ -1,4 +1,14 @@
 package com.example.joko.data.remote.response
 
-class AuthResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class AuthResponse(
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("refresh_token") val refreshToken: String,
+    @SerializedName("user") val user: UserResponse
+)
+
+data class UserResponse(
+    @SerializedName("id") val id: String,
+    @SerializedName("email") val email: String
+)
